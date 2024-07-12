f12d67122c9074e521528c69bcffb4a04e42899e
Lia Visintin <lia.visintin@gmail.com>
2024-07-12 11:34:20 +0200

    Add files via upload

---
+++ Input/Mycotoxin-PK.model.R
@@ -0,0 +1,131 @@
+# Generic mycotoxin model
+
+States  = { 
+  Q_GI,       # Quantity of mycotoxin in the GI compartment (nmol)
+  Q_fec,  # Quantity of mycotoxin in feces (nmol)
+  Qcpt,       # Quantity in central compartment (nmol)
+  Qu,     # Quantity of mycotoxin in urine (nmol)
+  Qmet,   # Quantity of metabolite central compartment (nmol)
+  Qu_met,     # Quantity of metabolite in urine (nmol)
+  AUC,         # AUC of central compartment (nmol-hr/L)
+};  
+
+Outputs = {
+  Ccpt, # Central compartment concentration
+  #Cmet, # Central compartment metabolite concentration
+  Ccpt_out, # Central compartment concentration (truncated at 1e-15)
+  #Cmet_out, # Central compartment metabolite concentration (truncated at 1e-15)
+  Qu_out, # Amount excreted in urine (truncated at 1e-15)
+  #Q_fec_out, # Amount excreted in feces (truncated at 1e-15)
+  Qu_met_out, # Amount of metabolite excreted in urine (truncated at 1e-15)
+};
+
+#population mean
+#M_lnkgut = -0.70; # Fraction absorbed
+M_lnFgutabs = 0.2;
+M_lnkgutelim = -1.05; # Gut elimination
+#M_lnktot = -1.02; # Total elimination
+M_lnkufrac = 0.32; # Fraction of elimination that is urine
+#M_lnkumet = 0.37; # Urinary excretion of metabolite
+M_lnVdist = 2.1; # Volume of distribution (L/kg)
+M_lnVdistmet = 0; # Volume of distribution for metabolite (L/kg)
+M_lnCltot = 0;
+M_lnClmet = 0;
+
+#population variance
+#SD_lnkgut = 0.2;
+SD_lnFgutabs = 0.2;
+SD_lnkgutelim = 0.2;
+#SD_lnktot = 0.2;
+SD_lnkufrac = 0.2;
+#SD_lnkumet = 0.2;
+SD_lnVdist = 0.2; 
+SD_lnVdistmet = 0.2;
+SD_lnCltot = 0.2;
+SD_lnClmet = 0.2;
+
+#individual log transformed, z-score
+#lnkgut = 0;
+lnFgutabs = 0;
+lnkgutelim = 0;
+#lnktot = 0;
+lnkufrac = 0;
+#lnkumet = 0;
+lnVdist = 0; 
+lnVdistmet = 0;
+lnCltot = 0;
+lnClmet =0;
+
+#individual parameters
+# Oral input modeling
+InitDose    = 50; # ingested input at t=0 (nmol)
+ConstDoseRate = 0; # Constant dose rate per hour (nmol/h) 
+#kgut    = 0.5; #intestinal elimination and absorption
+Fgutabs = 3133890;
+Fgutabs_tmp = 0.5; 
+kgutabs    = 0.35; # Intestinal absorption rate (/h); kgutelim * Fgutabs/(1-Fgutabs)
+Cltot = 0.5;
+Clmet = 0.5;
+
+# Distribution volumes (L/kg)
+Vdist = 0.74;
+Vdistmet = 1.0;
+
+# Body weight (kg)
+BW = 70;
+
+# Elimination rate constants (/h)
+#ktot      = 0.30;   # total elimination rate constant
+ku       = 0.32;     # Urinary excretion rate constant 
+ku_tmp   = 0.1;     # Set it to avoid the value geq ktot
+kmet       = 0.2;   # Metabolism rate constant 
+#kumet    = 0.37;     # Urinary excretion rate constant for metabolite
+kgutelim   = 0.35;     #gut elimination rate
+
+#GSD - Mesidual errors
+GSD_Ccpt = 1.1; # Central compartment concentration
+GSD_Cmet = 1.1; # Central compartment metabolite concentration
+GSD_Qu = 1.1;  # Amount excreted in urine
+GSD_Q_fec = 1.1;  # Amount excreted in feces
+GSD_Qu_met = 1.1; # Amount of metabolite excreted in urine
+
+Initialize {
+  Q_GI = InitDose;
+  #kgut = exp(M_lnkgut + SD_lnkgut * lnkgut);
+  Fgutabs = exp(M_lnFgutabs +SD_lnFgutabs * lnFgutabs);
+  Fgutabs = Fgutabs_tmp; #if gut abs tmp is bigger than 1 write 1, otherwise (:) write gut abs temp
+  kgutelim = exp(M_lnkgutelim + SD_lnkgutelim * lnkgutelim);
+  #kgutabs = kgut - kgutelim;
+  kgutabs = kgutelim * Fgutabs/(1-Fgutabs);
+  #ktot = exp(M_lnktot + SD_lnktot * lnktot);
+  #kumet = exp(M_lnkumet + SD_lnkumet * lnkumet);
+  Cltot = exp(M_lnCltot + SD_lnCltot * lnCltot);
+  Clmet = exp(M_lnClmet + SD_lnClmet * lnClmet);
+  Vdist = exp(M_lnVdist + SD_lnVdist * lnVdist);
+  ku_tmp = (Cltot/Vdist) * exp(M_lnkufrac + SD_lnkufrac * lnkufrac);
+  ku = ku_tmp;
+  kmet = (Cltot/Vdist) - ku;
+  Vdistmet = exp(M_lnVdistmet + SD_lnVdistmet * lnVdistmet);
+}
+
+Dynamics { 
+  dt (Q_GI)  = ConstDoseRate - Q_GI * (kgutabs + kgutelim);
+  dt (Q_fec) = Q_GI * kgutelim;
+  dt (Qcpt) = Q_GI * kgutabs - Qcpt * (ku + kmet);
+  dt (Qu) = Qcpt * ku;
+  dt (Qmet) = Qcpt * kmet - Qmet * (Clmet/Vdistmet);
+  dt (Qu_met) = Qmet * (Clmet/Vdistmet);
+  dt (AUC) = Qcpt/(Vdist*BW);
+}
+
+CalcOutputs { # truncate at Q values of 1e-15
+  Ccpt = Qcpt / (1000*Vdist*BW);
+  #Cmet = Qmet / (1000*Vdistmet*BW);
+  Ccpt_out = (Ccpt < 1e-15 ? 1e-15 : Ccpt);
+  #Cmet_out = (Cmet < 1e-15 ? 1e-15 : Cmet);
+  Qu_out = (Qu < 1e-15 ? 1e-15 : Qu);
+  #Q_fec_out = (Q_fec < 1e-15 ? 1e-15 : Q_fec);
+  Qu_met_out = (Qu_met < 1e-15 ? 1e-15 : Qu_met);
+}
+
+End.
---
+++ Input/Mycotoxin-default-run.in.R
@@ -0,0 +1,22 @@
+#Example run with default parameters
+Integrate (Lsodes, 1e-4, 1e-6, 1);
+OutputFile("Mycotoxin-default-run.out");
+
+M_lnFgutabs = -1.76;
+M_lnkgutelim = -0.7; # Gut elimination
+#M_lnktot = -3.96; # Total elimination
+M_lnkufrac = -0.7; # Fraction of elimination that is urine
+#M_lnkumet = -0.7; # Urinary excretion of metabolite
+M_lnCltot = -3.33;
+M_lnClmet = -3.33;
+M_lnVdist = -0.10; # Volume of distribution (L/kg)
+M_lnVdistmet = -0.10; # Volume of distribution for metabolite (L/kg)
+
+  Simulation {
+    InitDose    = 13172;    # ingested dose (ng)
+    BW         = 66;      # body weight (kg)
+
+    PrintStep(Ccpt_out, Qu_out, Qu_met_out, 0, 48, 0.1);
+  }
+
+End.
